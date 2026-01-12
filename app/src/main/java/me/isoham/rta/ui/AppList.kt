8e5c7f30bd204c1fd15d23ff56459838e4d91cff
iSoham <68643247+Zone-Infinity@users.noreply.github.com>
2026-01-12 14:59:34 +0530

    autofocus when at home

--- app/src/main/java/me/isoham/rta/ui/AppList.kt
+++ app/src/main/java/me/isoham/rta/ui/AppList.kt
@@ -1,17 +1,24 @@
 package me.isoham.rta.ui
 
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,17 +29,26 @@ fun AppList(
     apps: List<AppInfo>,
     onAppClick: (AppInfo) -> Unit
 ) {
-    Column {
-        var query by remember { mutableStateOf("") }
+    var query by remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 
+    Column {
         // Search bar
         TextField(
             value = query,
             onValueChange = { query = it },
-            placeholder = { Text("Search apps") },
             singleLine = true,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequester)
+                .height(if (query.isEmpty()) 1.dp else 56.dp)
+                .alpha(if (query.isEmpty()) 0f else 1f),
+            placeholder = { Text("Search apps") }
         )
+
 
         AndroidView(
             modifier = Modifier.fillMaxSize(),
